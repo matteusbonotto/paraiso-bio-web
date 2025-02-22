2ea10636b613b9119393486758b7b01ae5fb08f9
Matteus Bonotto <matteusbonotto+github@gmail.com>
2025-02-22 18:08:57 -0300

    add Component Accordion

---
+++ app/src/components/global/AccordionComponent/Accordion.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Accordion.css';
+
+const Accordion = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="accordion-item">
+      <div className="accordion-header" onClick={toggleAccordion}>
+        <h5>{title}</h5>
+        <span>{isOpen ? '-' : '+'}</span>
+      </div>
+      {isOpen && <div className="accordion-content">{children}</div>}
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Accordion;
--- app/src/pages/Contacts/contact.jsx
+++ app/src/pages/Contacts/contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import Talkto from '../../components/web/TalktoComponent/Talkto';
+import Accordion from '../../components/global/AccordionComponent/Accordion';
+
+const contacts = [
+  { name: "Matheus", phone: "(19)99959-1073", whatsapp: "(19)99959-1073" },
+  { name: "Beatriz", phone: "(19)99103-5924", whatsapp: "(19)99103-5924" },
+  { name: "Portaria", phone: "(19)99258-5597", whatsapp: "(19)99258-5597" },
+  { name: "Síndico Ricardo", phone: "(19)98289-0000", whatsapp: "(19)98289-0000" },
+  { name: "Subsíndica Paty", phone: "(19)98812-4347", whatsapp: "(19)98812-4347" },
+  { name: "Zelador", phone: "(19)98993-9419", whatsapp: "(19)98993-9419" },
+  { name: "Zuma Engenharia", phone: "(19)99816-0260", whatsapp: "(19)99816-0260" },
+  { name: "Seguro Tokyo Marine", phone: "(11)99578-6546", whatsapp: "(11)99578-6546" },
+];
 
 const Contact = () => {
   console.log('Contact component rendered');
-  return <div>Contact Paraiso Bio Web</div>;
+  return (
+    <div className='container p-4'>
+      <Accordion title="Contatos">
+        <Talkto contacts={contacts} />
+      </Accordion>
+    </div>    
+  );
 };
 
 export default Contact;
